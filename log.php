0a392c26d94fec240cf4107a03be33986e92aac3
Srivenkatesh1995 <51349263+Srivenkatesh1995@users.noreply.github.com>
2020-02-03 17:29:38 -0700

    Create log.php

---
+++ log.php
@@ -0,0 +1,8 @@
+<?php 
+// COMMIT THIS FILE TO THE REPO SO THAT IT'LL BE RIGHT THERE WHEN WE DEPLOY
+$filename = './sample_species_list_data_based_request_log.csv';
+
+$testArray = array("A,A,A,A,A,A,A,A,A");
+file_put_contents('./sample_species_list_data_based_request_log.csv',$testArray);
+
+?>
